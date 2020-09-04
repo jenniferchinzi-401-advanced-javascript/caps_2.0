9a5448adecf414118e0ea58829017d55b08d2535
Jennifer Chinzi <jenrose87@gmail.com>
2020-09-03 19:48:59 -0700

    Lab 19 - not working perfectly with API Server but queue is functional

--- driver/driver.js
+++ driver/driver.js
@@ -1,22 +1,15 @@
-/*
-Connects to the CAPS server as a socket.io client to the caps namespace
-Listen for the pickup event coming in from the CAPS server
-  - Simulate picking up the package
-    - Wait 1.5 seconds
-    - Log “picking up payload.id” to the console
-    - emit an in-transit event to the CAPS server with the payload
-  - Simulate delivering the package
-    - Wait 3 seconds
-    - emit a delivered event to the CAPS server with the payload
-*/
-
 'use strict';
 
 const io = require('socket.io-client');
 
-const socket = io.connect('http://localhost:3000');
+const socket = io.connect('http://localhost:3000/caps');
+
+socket.emit('get-all');
 
 socket.on('pickup', payload => {
+
+  socket.emit('received', payload.orderId);
+
   setTimeout(() => {
     console.log(`DRIVER: picked up ${payload.orderId}`);
     socket.emit('in-transit', payload);
@@ -26,6 +19,7 @@ socket.on('pickup', payload => {
     console.log(`DRIVER: delivered ${payload.orderId}`);
     socket.emit('delivered', payload);
   }, 3000);
+
 });
 
 
